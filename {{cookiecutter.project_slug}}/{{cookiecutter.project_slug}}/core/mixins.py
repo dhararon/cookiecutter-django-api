dffd375345be0b9898d00925e6286dc15f5ad34c
Dharwin Perez <dhararon@hotmail.com>
2018-08-30 11:42:49 -0500

    Fix mixin and error_handle

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/mixins.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/mixins.py
@@ -5,13 +5,13 @@ from rest_framework.response import Response
 
 
 class BaseModelMixin(object):
-    def get_response_data(self, data, *args, **kwargs):
+    def get_response_data(self, serializer, *args, **kwargs):
         """
             Get the data to return as response data
             By default it returns `serializer.data`
             Override this method to return custom response data
         """
-        return data
+        return serializer.data
 
 
 class CreateModelMixin(BaseModelMixin, mixins.CreateModelMixin):
@@ -23,7 +23,7 @@ class CreateModelMixin(BaseModelMixin, mixins.CreateModelMixin):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
-        response_data = self.get_response_data(serializer.data)
+        response_data = self.get_response_data(serializer)
         return Response(
             response_data, status=status.HTTP_201_CREATED)
 
@@ -46,5 +46,5 @@ class UpdateModelMixin(BaseModelMixin, mixins.UpdateModelMixin):
             # forcibly invalidate the prefetch cache on the instance.
             instance._prefetched_objects_cache = {}
 
-        response_data = self.get_response_data(serializer.data)
+        response_data = self.get_response_data(serializer)
         return Response(response_data)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/utils.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/core/utils.py
@@ -4,6 +4,7 @@ from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
+
     if response is None:
         return response
 
@@ -23,14 +24,24 @@ def custom_exception_handler(exc, context):
 
     else:
         error_list = []
-        for field in response.data.keys():
-            error = {
-                "field": field,
-                "message": response.data[field].get(
-                    'message', 'Internal Error 1.'),
-                "code": int(response.data[field].get('code', 500))
-            }
-            error_list.append(error)
+        for field, values in response.data.items():
+            if isinstance(values, list):
+                values = values[0]
+                error = {
+                    "field": field,
+                    "message": str(values),
+                    "code": getattr(values, 'code', 500)
+                }
+                error_list.append(error)
+
+            else:
+                error = {
+                    "field": field,
+                    "message": values.get(
+                        'message', 'Internal Error 1.'),
+                    "code": int(values.get('code', 500))
+                }
+                error_list.append(error)
 
         response.data = {
             "body": {},
